5c1435c0c841353313e8ddeeb978a4dc37901782
shmick <github@warning.ca>
2015-02-22 13:21:51 -0500

    Updated net-watchdog.sh

--- net-watchdog.sh
+++ net-watchdog.sh
@@ -3,16 +3,18 @@
 # /mnt/mmcblk0p4/net-watchdog.sh
 
 # crontab entry
-# */3 * * * *  /mnt/mmcblk0p4/net-watchdog.sh
+#*/10 * * * *  /mnt/mmcblk0p4/net-watchdog.sh
 
-test_host=$(netstat -nr | grep "UG" | awk '{ print $2}' | xargs ping -q -w 1 -c 1 | grep "received" | awk '{ print $4 }')
+test_host=$(netstat -nr | grep "UG" | awk '{print $2}' | xargs ping -q -w 1 -c 1 | grep "received" | awk '{print $4}')
 if [ "$test_host" == "0" ] || [ -z "$test_host" ] ;
 then
+logger -p user.alert "Restarting network"
 /etc/init.d/network reload
-sleep 60
-test_host=$(netstat -nr | grep "UG" | awk '{ print $2}' | xargs ping -q -w 1 -c 1 | grep "received" | awk '{ print $4 }')
+sleep 120
+test_host=$(netstat -nr | grep "UG" | awk '{print $2}' | xargs ping -q -w 1 -c 1 | grep "received" | awk '{print $4}')
 	if [ "$test_host" == "0" ] || [ -z "$test_host" ] ;
 	then
+	logger -p user.alert "Rebooting"
 	/sbin/reboot
 	fi
 fi
