0aa155dc666cc71e8bf7daa2c44ab5cca4801317
shmick <github@warning.ca>
2015-02-23 09:12:46 -0500

    Formatting updates

--- pushbullet.sh
+++ pushbullet.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+TYPE="Content-Type: application/json"
 PB_KEY="Pushbullet_Access_Token"
 PB_HOST="https://api.pushbullet.com/v2/pushes"
-PB_MSG="{\"type\": \"note\", \"title\": \"HM Alert: $pn: $pcurr\", \"body\": \"$pn Alert: Alarm $al_type outside threshold $al_thresh, currently $pcurr\"}"
-curl -k -o /dev/null -s -H "Content-Type: application/json" -u "$PB_KEY": -d "$PB_MSG" $PB_HOST
+PB_MSG="{ \
+\"type\": \"note\", \
+\"title\": \"HM Alert: $pn: $pcurr\", \
+\"body\": \"$pn Alert: Alarm $al_type outside threshold $al_thresh, currently $pcurr\" \
+}"
+curl -k -o /dev/null -s -H "$TYPE" -u "$PB_KEY": -d "$PB_MSG" $PB_HOST
--- thingspeak.sh
+++ thingspeak.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-curl -q -k -d "api_key=MY_THINGSPEAK_TWITTER_API_KEY" -d "status=$pn Alert: Alarm $al_type outside threshold $al_thresh, currently $pcurr" https://api.thingspeak.com/apps/thingtweet/1/statuses/update
+TS_KEY="api_key=MY_THINGSPEAK_TWITTER_API_KEY"
+TS_MSG="status=$pn Alert: Alarm $al_type outside threshold $al_thresh, currently $pcurr"
+TS_SRV="https://api.thingspeak.com/apps/thingtweet/1/statuses/update"
+curl -q -k -d "$TS_KEY" -d "$TS_MSG" "$TS_SRV"
